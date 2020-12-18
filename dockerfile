149c75864cd131edf82989c73ee5d1106383427e
hjq133 <hjq121200@gmail.com>
2020-12-18 08:18:37 +0800

    add all

--- dockerfile
+++ dockerfile
@@ -1,3 +1,6 @@
-FROM python:3.7
-WORKDIR /app/
-COPY ./* ./
+# gradle 好大
+FROM gradle:jdk14
+WORKDIR /app
+COPY build.gradle gradle settings.gradle miniplc0-java.iml /app/
+COPY src /app/src
+RUN gradle fatjar --no-daemon
---
+++ src/main/java/miniplc0java/instruction/Operation.java
@@ -0,0 +1,20 @@
+package miniplc0java.instruction;
+
+public enum Operation {
+    ILL,
+    LIT,
+    LOD,
+    STO,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    EQ,
+    NEQ,
+    LT, // <
+    GT, // >
+    LE, // <=
+    GE, // >=
+    CALL,
+    WRT
+}
